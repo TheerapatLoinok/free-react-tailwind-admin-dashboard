d04d58420c342f6523642fe40be61de3eeba4ab4
theerapatloinok <theerapat.l@kkumail.com>
2024-05-23 21:02:52 +0700

    [feature] clear file that never use

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -243,6 +243,17 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                                 Train data
                               </NavLink>
                             </li>
+                            <li>
+                              <NavLink
+                                to="/admin/settings/uploadfiles"
+                                className={({ isActive }) =>
+                                  'group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ' +
+                                  (isActive && '!text-white')
+                                }
+                              >
+                                Preview data
+                              </NavLink>
+                            </li>
                             {role === 'admin-dev' && (
                               <>
                                 <li>
